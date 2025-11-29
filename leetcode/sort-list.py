5ba63b45fe20095770e1e7d0bbaf1f2e54e714cf
AhmedHashim27 <115380609+AhmedHashim27@users.noreply.github.com>
2025-11-30 01:49:48 +0200

    Update solution for sort-list

--- leetcode/sort-list.py
+++ leetcode/sort-list.py
@@ -9,18 +9,62 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        if not head: return None
 
-        values = []
-        curr = head
-        while curr:
-            values.append(curr.val)
-            curr = curr.next
 
-        values.sort()
+        if not head or not head.next:
+            return head
 
-        curr = head
-        for v in values:
-            curr.val = v
-            curr = curr.next
-        return head
+        left = head
+        right = self.getmed(head)
+
+        temp = right.next
+        right.next = None
+        right = temp
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+
+        return self.merge(left, right)
+
+
+    def getmed(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow 
+
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        tail = dummy
+    
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail= tail.next
+        if l1: tail.next = l1
+        if l2: tail.next = l2
+        return dummy.next
+
+
+
+
+        # if not head: return None
+
+        # values = []
+        # curr = head
+        # while curr:
+        #     values.append(curr.val)
+        #     curr = curr.next
+
+        # values.sort()
+
+        # curr = head
+        # for v in values:
+        #     curr.val = v
+        #     curr = curr.next
+        # return head
